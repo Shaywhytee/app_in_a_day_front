50d5be92a0fdddc7eaf66d914ef41633a4e767ec
Raji Johnson <rjohnson1906@gmail.com>
2023-04-20 15:29:14 -0400

    Message Test

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
-// Random Number Generator for Spirits/Mixers
-function generateRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const min = 1;
-const max = 10;
-const baseDrinkOpt = generateRandomNumber(min, max);
-const baseMixersOpt = generateRandomNumber(min, max);
-
-console.log(baseDrinkOpt);
-console.log(baseMixersOpt);
-
-
-
 
 
