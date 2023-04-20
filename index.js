4ba25c32489e107d0bf8ece1604275932eac5384
Colton Kresser <kressercolton12@gmail.com>
2023-04-20 16:05:17 -0600

    Updated HTML AND css

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 
 // List of Base Drinks & Mixers
 let baseSpirits = [
-  {name: "Vodka"},
-  {name: "Whiskey"},
-  {name: "Bourbon"},
-  {name: "Tequila"},
-  {name: "Gin"},
-  {name: "Rum"},
-  {name: "Brandy"},
-  {name: "Beer"},
-  {name: "Wine"},
-  {name: "Scotch"},
-  ];
+    {name: "Vodka"},
+    {name: "Whiskey"},
+    {name: "Bourbon"},
+    {name: "Tequila"},
+    {name: "Gin"},
+    {name: "Rum"},
+    {name: "Brandy"},
+    {name: "Beer"},
+    {name: "Wine"},
+    {name: "Scotch"},
+    ];
 
 let baseMixers = [
-  {name: "Coke"},
-  {name: "Cranberry Juice"},
-  {name: "Orange Juice"},
-  {name: "Tonic"},
-  {name: "Red Bull"},
-  {name: "Iced Tea"},
-  {name: "Cold Brew"},
-  {name: "Pineapple Juice"},
-  {name: "Coconut Water"},
-  {name: "Ginger Ale"},
-      ];
+    {name: "Coke"},
+    {name: "Cranberry Juice"},
+    {name: "Orange Juice"},
+    {name: "Tonic"},
+    {name: "Red Bull"},
+    {name: "Iced Tea"},
+    {name: "Cold Brew"},
+    {name: "Pineapple Juice"},
+    {name: "Coconut Water"},
+    {name: "Ginger Ale"},
+        ];
 
 // Random Number Generator for Spirits/Mixers
 function generateRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // Get Name Function
 function getName(array) {
-const index = generateRandomNumber(0, array.length - 1);
-return array[index].name;
+  const index = generateRandomNumber(0, array.length - 1);
+  return array[index].name;
 }
 
 let baseSpiritsOpt
@@ -44,65 +44,79 @@ console.log(baseSpiritsOpt);
 console.log(baseMixersOpt);
 
 
-loginButton.addEventListener("click", function() {
-  const email = emailInput.value;
-  const password = passwordInput.value;
+const textWrapper = document.querySelector(".text_wrapper");
 
-  // Login Fetch
-  fetch("http://127.0.0.1:5000/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      user_email: emailInput.value,
-      user_password: passwordInput.value
+
+const startBtn = document.getElementById("start");
+startBtn.addEventListener("click", () => {
+   textWrapper.innerHTML = ""
+    baseSpiritsOpt = getName(baseSpirits);
+    baseMixersOpt =  getName(baseMixers);
+    const results = document.createElement("p");
+    results.textContent = `${baseSpiritsOpt} and ${baseMixersOpt}`
+   textWrapper.appendChild(results)
+})
+
+
+loginButton.addEventListener("click", function() {
+    const email = emailInput.value;
+    const password = passwordInput.value;
+  
+    // Login Fetch
+    fetch("http://127.0.0.1:5000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        user_email: emailInput.value,
+        user_password: passwordInput.value
+      })
     })
-  })
-  .then(response => {
-    if (response.ok) {      
-      alert("Login was succesful!")
-      window.location.href="/index"
-    } else {
-      throw new Error("Login failed, please try again");
-    }
-  })
-  .catch(error => {
-    console.error("Error:", error);
+    .then(response => {
+      if (response.ok) {      
+        alert("Login was succesful!")
+        window.location.href="/index"
+      } else {
+        throw new Error("Login failed, please try again");
+      }
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
   });
-});
 
 
 //   Account Creation Fetch Call
 accountButton.addEventListener("click", () => {
-  const newEmail = emailInput.value;
-  const newPassword = passwordInput.value;
-  const verfNewPassword = verfNewPasswordInput.value;
+    const newEmail = emailInput.value;
+    const newPassword = passwordInput.value;
+    const verfNewPassword = verfNewPasswordInput.value;
 
-  if (passwordInput.value != verfNewPassword.value) {
-      alert("Error: Passwords do not match!")
+    if (passwordInput.value != verfNewPassword.value) {
+        alert("Error: Passwords do not match!")
 
-  } else {
-      fetch("http://127.0.0.1:5000/account/create", {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json"
-          },
-          body: JSON.stringify({
-              user_email: emailInput.value,
-              user_password: passwordInput.value
-          })
-          .then(response => {
-              if (response.ok) {
-                  alert("New account created!")
-                  window.location.href=("/login")
-              } else {
-                  throw new Error("Account creation failed! Please try again...")
-              }
-          }) 
-          .catch(error => {
-              console.log("Error:", error)
-          })
-      })
-    }  
-  });
+    } else {
+        fetch("http://127.0.0.1:5000/account/create", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                user_email: emailInput.value,
+                user_password: passwordInput.value
+            })
+            .then(response => {
+                if (response.ok) {
+                    alert("New account created!")
+                    window.location.href=("/login")
+                } else {
+                    throw new Error("Account creation failed! Please try again...")
+                }
+            }) 
+            .catch(error => {
+                console.log("Error:", error)
+            })
+        })
+      }  
+    });
