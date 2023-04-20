a2faf2230e685a0b8f3c2e95c7aee6ef2875faa4
Raji Johnson <rjohnson1906@gmail.com>
2023-04-20 16:03:31 -0400

    getName Function

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
+// List of Base Spirits & Mixers
+let baseSpirits = [
+  {name: "Vodka"},
+  {name: "Whiskey"},
+  {name: "Bourbon"},
+  {name: "Tequila"},
+  {name: "Gin"},
+  {name: "Rum"},
+  {name: "Brandy"},
+  {name: "Beer"},
+  {name: "Wine"},
+  {name: "Scotch"},
+  ];
+
+let baseMixers = [
+  {name: "Coke"},
+  {name: "Cranberry Juice"},
+  {name: "Orange Juice"},
+  {name: "Tonic"},
+  {name: "Red Bull"},
+  {name: "Iced Tea"},
+  {name: "Cold Brew"},
+  {name: "Pineapple Juice"},
+  {name: "Coconut Water"},
+  {name: "Ginger Ale"},
+      ];
+
 // Random Number Generator for Spirits/Mixers
 function generateRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const min = 1;
-const max = 10;
-const baseDrinkOpt = generateRandomNumber(min, max);
-const baseMixersOpt = generateRandomNumber(min, max);
+// Get Name Function
+function getName(array) {
+  const index = generateRandomNumber(0, array.length - 1);
+  return array[index].name;
+}
 
-console.log(baseDrinkOpt);
+const baseSpiritsOpt = getName(baseSpirits);
+const baseMixersOpt = getName(baseMixers);
+
+console.log(baseSpiritsOpt);
 console.log(baseMixersOpt);
-
-
-
 
 
 //   Account Creation Fetch Call
